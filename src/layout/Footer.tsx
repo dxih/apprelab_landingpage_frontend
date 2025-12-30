c320eafdc81f2504905d607bca374601e66a3624
Kamsiyochukwu Mebuge <brendanmebson@gmail.com>
2025-12-30 08:57:07 -0800

    few changes

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/newsletter", {
+      const res = await fetch("/api/newsletter", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
